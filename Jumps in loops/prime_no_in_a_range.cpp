2ce6a9ebe243a4851c0249e12b26baedb6c7204f
Sandesh <sandeshbansal24@gmail.com>
2021-07-30 15:52:23 +0530

    commit

---
+++ Jumps in loops/prime_no_in_a_range.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int j;
+    for(int i=a;i<=b;i++) {
+        for(j=2;j<i;j++) {
+            if(i%j==0) {
+                break;
+            }
+        }
+        if(j==i) {
+        cout<<i<<endl;
+        }
+    }
+    return 0;
+}
